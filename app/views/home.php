86170eb0b657bfbf3d065bcef27ee731001d0db4
Dylano Hartman <dylano.hartman@gmail.com>
2018-04-10 23:49:31 +0200

    small changes

--- app/views/home.php
+++ app/views/home.php
@@ -4,8 +4,8 @@
     <p>Sorry, no pages at the moment.</p>
   <?php  else: ?>
     <div class="courses">
-      <?php shuffle($pages); ?>
-      <?php print_r($pages); ?>
+      <?php //shuffle($pages); ?>
+      <?php //print_r($pages); ?>
       <?php foreach($pages as $page): ?>
           <a href="<?php echo BASE_URL;?>/page.php?page=<?php echo $page['slug'];?>"  class="course <?php echo $page['slug']; ?>">
           <div>
