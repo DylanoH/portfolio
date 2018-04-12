69305bb9dc0e0c5f1e95a6e03e7c268d569b8963
Dylano-laptop <dylano.hartman@gmail.com>
2018-04-12 14:50:59 +0200

    update

--- app/views/page/show.php
+++ app/views/page/show.php
@@ -10,12 +10,24 @@
     <?php echo e($page['body']); ?>
   </p>
 
-  <p class="faded">
-    Created on <?php echo $page['created']->format('d M Y'); ?>
-    <?php if($page['updated']): ?>
-      Last updated <?php echo $page['updated']->format('d M Y'); ?>
-    <?php endif; ?>
-  </p>
+  <?php endif; ?>
+
+  <?php if(!$content): ?>
+    <p>No content found, sorry.</p>
+  <?php else: ?>
+
+    <h2><?php echo e($content['title']); ?></h2>
+
+    <p>
+      <?php echo e($content['content']); ?>
+    </p>
+
+  <!-- <p class="faded">
+    Created on <?php// echo $page['created']->format('d M Y'); ?>
+    <?php //if($page['updated']): ?>
+      Last updated <?php //echo $page['updated']->format('d M Y'); ?>
+    <?php //endif; ?>
+  </p> -->
 
 <?php endif; ?>
 
--- page.php
+++ page.php
@@ -13,18 +13,36 @@
       WHERE slug = :slug
     ");
 
+
+
+
     $page->execute(['slug' => $slug]);
 
     $page = $page->fetch(PDO::FETCH_ASSOC);
 
-    if ($page) {
-      $page['created'] = new DateTime($page['created']);
+    //
+    // if ($page) {
+    //   $page['created'] = new DateTime($page['created']);
+    //
+    //   if ($page['updated']) {
+    //     $page['updated'] = new DateTime($page['updated']);
+    //   }
+    // }
 
-      if ($page['updated']) {
-        $page['updated'] = new DateTime($page['updated']);
-      }
-    }
+    $vak = $_GET['page'];
+
+    $content = $con->prepare("
+      SELECT *
+      FROM course
+
+    ");
+
+    $content->execute();
+
+    $content = $content->fetch(PDO::FETCH_ASSOC);
   }
+
+
 
   require VIEW_ROOT . '/page/show.php';
 
