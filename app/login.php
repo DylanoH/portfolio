c5f409b9bc45ab6d705814f52396ea40b07a574b
Dylano Hartman <dylano.hartman@gmail.com>
2018-05-21 23:23:09 +0200

    login styling

--- app/login.php
+++ app/login.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>My CMS</title>
+    <title>Login</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
@@ -12,14 +12,20 @@
 
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/stylesheets/style.css">
   </head>
-
   <body>
-    <main>
-      <form class="formLogin" autocomplete="off">
-        <input type="text" id="username" name="username" placeholder="username"  autofocus required>
-        <input type="password" id="password" name="password" placeholder="password"  required>
-        <input type="submit" id="submitLogin" name="submitLogin" value="LogIn">
-      </form>
+    <main class="login">
+      <form class="formLogin" autocomplete="off" method="post">
+        <label for="title">
+          <h2>ACCOUNT LOGIN</h2>
+        </label>
+        <label for="username">
+          <input type="text" id="username" name="username" placeholder="username"  autofocus required>
+        </label>
+        <label for="password">
+          <input type="password" id="password" name="password" placeholder="password"  required>
+        </label>
+          <input type="submit" id="submitLogin" name="submitLogin" value="Login">
+        </form>
     </main>
   <script src="<?php echo BASE_URL; ?>/scripts/jquery/jquery.easing.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
